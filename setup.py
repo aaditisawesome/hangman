f297cf3f291c599b37554fa755b490559a21773d
Aadit Bansal <aaditisawesome@gmail.com>
2021-01-13 10:55:40 -0800

    v0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="AaditsHangman", # Replace with your own username
-    version="0.0.2",
+    version="0.0.3",
     author="Aadit Bansal",
     author_email="aaditisawesome@gmail.com",
     description="A hangman game created on tkinter python",
